c4402fa37da9059c26307066db02c882288c8a2d
Chris Kelley <kelleyc718@gmail.com>
2018-01-26 12:33:42 -0600

    Finished JS Exercise

--- js_exercises/boolean_order.js
+++ js_exercises/boolean_order.js
@@ -1,6 +1,32 @@
-function solve (s, ops) {
-  return 0;
+var s = 'ttfftff';
+
+function splitter (str) {
+  var booArr = str.split('');
+  return booArr;
 }
 
+var myArr = splitter(s);
+console.log(myArr);
 
-var and = (s && ops);
+function booAssign (myArr) {
+  var newArr = [];
+  for (var i = 0; i < myArr.length; i++) {
+    (myArr[i] === 't') ? newArr.push(true) : newArr.push(false);
+  }
+  return newArr;
+}
+
+var booVals = booAssign(myArr);
+
+console.log(booVals);
+
+var ops = '|&^&&';
+
+function opSplitter (ops) {
+  var opsArr = ops.split('');
+  return opsArr;
+}
+
+var opsArr = opSplitter(ops);
+
+console.log(opsArr);
