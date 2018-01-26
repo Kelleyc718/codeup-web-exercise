0817c9151c022301a781835652642495633b4ae5
Chris Kelley <kelleyc718@gmail.com>
2018-01-26 16:53:59 -0600

    Finished function drills

--- js/function_drills.js
+++ js/function_drills.js
@@ -121,47 +121,159 @@ function hasCapitalLetters (string) {
 }
 
 // Make a function named isLowerCase(letter)
+function isLowerCase (letter) {
+  return letter !== '' && letter.toLowerCase() === letter;
+}
+
+console.log(isLowerCase(''));
 
 // Make a function named hasLowerCase(string) that returns if a string has any lower cased
+function hasLowerCase(string) {
+  for (var i = 0; i < string.length; i++) {
+    if (isLowerCase(string[i])) {
+      return true;
+    }
+  }
+  return false;
+}
 
 // Make a function named isSpace(letter) that returns if a character is a space character
+function isSpace (letter) {
+  return letter === ' ';
+}
+
+console.log(isSpace(' '));
 
 // Make a function named hasSpaces(string) that returns if a string has any space characters
+function hasSpaces(string) {
+  return string.includes(' ');
+}
+
+console.log(hasSpaces('I am a string'));
 
 // Make a function named isZero(number)
+function isZero (number) {
+  return number === 0;
+}
 
 // Make a function named increment(number) that returns a number plus one
+function increment (number) {
+  return number++;
+}
 
 // Make a function named decrement(number) that returns a number minus one
+function decrement (number) {
+  return number--;
+}
 
 // Make a function named isFive(input)
+function isFive (input) {
+  return typeof input === 'number' && input === 5;
+}
 
 // Make a function named addFive(input) that adds five to some input.
+function addFive (input) {
+  input += 5;
+  return input;
+}
+
+console.log(addFive(3));
 
 // Make a function named isMultipleOfFive(input)
+function isMultipleOfFive (input) {
+  return input % 5 === 0;
+}
+
+console.log(isMultipleOfFive(500));
 
 // Make a function named isThree(input)
+function isThree (input) {
+  return typeof input === 'number' && input === 3;
+}
 
 // Make a function named isMultipleOfThree(input)
+function isMultipleOfThree (input) {
+  return input % 3 === 0;
+}
 
 // Make a function named isMultipleOfThreeAndFive(input)
+function isMultipleOfThreeAndFive (input) {
+  return input % 15 === 0;
+}
+
+console.log(isMultipleOfThreeAndFive(30));
 
 // Write a function named lowerCase(string)
+function lowerCase (string) {
+  return string.toLowerCase();
+}
 
 // Write a function named capitalizeEachWord(string) uses capitalize to return a string where each new word (separated by a space) is capitalized.
+function capitalizeEachWord (string) {
+  var newArr = [];
+  var arrWords = string.split(' ');
+  for (var i = 0; i < arrWords.length; i++) {
+    newArr.push(arrWords[i][0].toUpperCase() + arrWords[i].slice(1).toLowerCase());
+  }
+  return newArr.join(' ');
+}
+
+console.log(capitalizeEachWord('Here is my string'));
 
 // Write a function named add(a, b) that returns the sum of a and b
+function add (a, b) {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  }
+  return 'Inputs are not valid numbers.';
+}
 
 // Write a function named subtract(a, b) that returns a minus b
-
+function subtract (a, b) {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a - b;
+  }
+  return 'Inputs are not valid numbers.';
+}
 // Write a function named multiply(a, b) that returns the product of a times b
+function multiply (a, b) {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a * b;
+  }
+  return 'Inputs are not valid numbers.';
+}
 
 // Write a function named divide(a, b) that returns a divided by b
+function divide (a, b) {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a / b;
+  }
+  return 'Inputs are not valid numbers.';
+}
 
 // Write a function named remainder(a, b) that returns the remainder after dividing a by b
+function remainder (a, b) {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a % b;
+  }
+  return 'Inputs are not valid numbers.';
+}
 
 // Make a function named isBlank(input) that determines if a given input is spaces, newline characters, or tabs.
+function isBlank (input) {
+  return (/\s/.test(input));
+}
+
+console.log(isBlank(' '));
 
 // Make a function named isNumeric(input) returns true/false if the input is a number or numeric string
+function isNumber (input) {
+  return !isNaN(input);
+}
+
+console.log(isNumber('5'));
 
 // Make a function named trim(string) that removes empty spaces before and after the input.
+function trim (string) {
+  return string.trim();
+}
