a8eeeda6c6eace26079b0b3917e52e2996247f47
Chris Kelley <kelleyc718@gmail.com>
2018-01-26 00:18:36 -0600

    add files from exercies

--- js/external.js
+++ js/external.js
@@ -1,1 +1,9 @@
 'use strict';
+
+console.log('Hello from external javascript');
+
+window.alert('Welcome to my Website!');
+
+var usrInput = prompt('What is your favorite color?');
+
+window.alert('Wow! ' + usrInput + ' my favorite color too!');
