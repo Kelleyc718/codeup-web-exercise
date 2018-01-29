f25b948ec33d4bc836e1d565d1e6ce0e1ddad56e
Chris Kelley <kelleyc718@gmail.com>
2018-01-29 14:08:00 -0600

    Added bonus questions to conditionals.js

--- js/conditionals.js
+++ js/conditionals.js
@@ -173,3 +173,29 @@ alert(calculateTotal(discountLevel, totalPrice));
  */
 // Generate a random number between 0 and 6
 // var luckyNumber = Math.floor(Math.random() * 6);
+
+
+// Bonus Problems
+
+function dayOfWeekStartsWithT (str) {
+  return (typeof str === 'string' && str.startsWith('T'));
+}
+
+console.log(dayOfWeekStartsWithT('Tuesday'));
+console.log(dayOfWeekStartsWithT('Saturday'));
+
+function isPalindrome (str) {
+  var wordLength = Math.floor(str.length / 2);
+  for (var i = 0; i < wordLength; i++) {
+    if (str[i] !== str[str.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isValidPassword (password) {
+  return (password.length === 6 && (/^[a-zA-Z0-9]+$/g).test(password) && !isPalindrome(password));
+}
+
+console.log(isValidPassword('ffggff'));
