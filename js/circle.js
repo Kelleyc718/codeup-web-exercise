38c0dceb5a95dd781965f5b2cba46b547d3a7097
Chris Kelley <kelleyc718@gmail.com>
2018-02-02 13:32:49 -0600

    Complete math and bom exercises

--- js/circle.js
+++ js/circle.js
@@ -14,24 +14,24 @@
       // TODO: complete this method.
       // If doRounding is true, round the result to the nearest integer.
       // Otherwise, output the complete value
-      var round = (doRounding) ? Math.round(circle.getArea(circle.radius)) : Math.ceil(circle.getArea(circle.radius));
+      var round = (doRounding) ? Math.round(this.getArea(this.radius)) : this.getArea(this.radius);
       console.log(`Area of a circle with radius: ${this.radius}, is: ${round}`);
     }
   };
 
     // log info about the circle
-    console.log("Raw circle information");
-    circle.logInfo(false);
-    console.log("Circle information rounded to the nearest whole number");
-    circle.logInfo(true);
+  console.log('Raw circle information');
+  circle.logInfo(false);
+  console.log('Circle information rounded to the nearest whole number');
+  circle.logInfo(true);
 
-    console.log("=======================================================");
-    // TODO: Change the radius of the circle to 5.
+  console.log('=======================================================');
+  // TODO: Change the radius of the circle to 5.
   circle.radius = 5;
   circle.getArea(circle.radius);
     // log info about the circle
-    console.log("Raw circle information");
-    circle.logInfo(false);
-    console.log("Circle information rounded to the nearest whole number");
-    circle.logInfo(true);
+  console.log('Raw circle information');
+  circle.logInfo(false);
+  console.log('Circle information rounded to the nearest whole number');
+  circle.logInfo(true);
 })();
--- js/validation.js
+++ js/validation.js
@@ -1,18 +1,17 @@
-var username = document.forms.login.username;
-var password = document.forms.login.password;
-// Username and Password Length Validation
-var passLen = password.length >= 5;
-var nameLen = username.length < 20;
-
-// Username and Password does not include invalid chars
-var passName = !password.includes(username);
-var whiteSpace = !username.startsWith(' ') && !password.startsWith(' ') && !username.endsWith(' ') && !password.startsWith(' ');
-
-// Const Variable to test all cases defined above
-const valid = passLen && passName && nameLen && whiteSpace;
-
-function validation (username, password) {
-  return (valid === true) ? 'success' : 'denied';
-}
-
-console.log(validation(username, password));
+(function () {
+  'use strict';
+  function validate () {
+    var usrName = document.forms.login.username.value;
+    var psw = document.forms.login.password.value;
+    // Username and Password Length Validation
+    var passLen = psw.length >= 5;
+    var nameLen = usrName.length < 20;
+    if ((usrName && psw === '')) {
+      alert('Both fields must be filled out.');
+    }
+    var passName = !psw.includes(usrName);
+    var whiteSpace = !usrName.startsWith(' ') && !psw.startsWith(' ') && !usrName.endsWith(' ') && !psw.startsWith(' ');
+    if (passLen && nameLen && passName && whiteSpace) {
+    }
+  }
+})();
