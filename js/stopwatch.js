c9f75b776bf097b3adb8601d6cfe7d2c523714dd
Chris Kelley <kelleyc718@gmail.com>
2018-02-05 20:32:36 -0600

    Stopwatch bonus and google maps api complete

---
+++ js/stopwatch.js
@@ -0,0 +1,7 @@
+(function () {
+  'use strict';
+  const timer = document.getElementById('timer');
+  const startBtn = document.getElementById('start-lap');
+  const stopBtn = document.getElementById('stop');
+  const resetBtn = document.getElementById('reset');
+})();
