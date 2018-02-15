b99e9ba3c21304a5fe6551e0f6c22053b5fa8605
Chris Kelley <kelleyc718@gmail.com>
2018-02-15 16:12:22 -0600

    Finish outline for weather/map api

--- deploy/tictactoe/js/game.js
+++ deploy/tictactoe/js/game.js
@@ -1,45 +1,65 @@
-'use strict';
-var cells = $('.cell');
-var playerOne = [];
-var playerTwo = [];
+(function () {
+  'use strict';
+  var cells = $('.cell');
+  var playerOne = 1;
+  var cpu = 2;
+  var board = {};
+  board.state = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  board.empty = 0;
+  var win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
-let count = 0;
-
-cells.click(function () {
-  if ($(this).html() !== '') {
-    alert('That Move is invalid');
-  } else {
-    playerMove($(this));
+  function moves () {
+    var movesPlayed = [];
+    console.log(movesPlayed);
+    for (let i = 0; i < 9; i++) {
+      if (board.state[i] === board.empty) {
+        movesPlayed.push(i);
+      }
+    }
+    return movesPlayed;
   }
-});
 
+  let count = 0;
+  const turn = function () {
+    if (count % 2 === 0) {
+      return playerOne;
+    }
+    return cpu;
+  };
 
-const playerMove = player => {
-  return (count % 2 !== 0) ? ($(this).html('O'), playerTwo.push($(this).data('num')), count++, clickCount(count)) : ($(this).html('X'), playerOne.push($(this).data('num')), count++, clickCount(count));
-};
-
-const clickCount = count => {
-  if (count === 9) {
-    draw(count);
+  function full () {
+    for (let i = 0; i < 9; i++) {
+      return (board.state[i] === board.empty);
+    }
   }
-};
 
-const draw = count => {
-  return (count === 9 && !win()) ? console.log('Draw') : null;
-};
-const win = (playerOne, playerTwo) => {
-  switch (playerOne || playerTwo === 15) {
-    case cells += (parseInt(cells[0]) +
-                  parseInt(cells[1]) +
-                  parseInt(cells[2])) :
-      break;
-    case cells += (parseInt(cells[3]) +
-                  parseInt(cells[4]) +
-                  parseInt(cells[5])) :
-      break;
-    case cells += (parseInt(cells[6]) +
-                  parseInt(cells[7]) +
-                  parseInt(cells[8])) :
-      break;
+  function checkWin () {
+    for (var i = 0; i < 9; i++) {
+      var a = board.state[win[i][0]];
+      console.log(a);
+      var b = board.state[win[i][1]];
+      console.log(b);
+      var c = board.state[win[i][2]];
+      console.log(c);
+      if (a === b && a === c && a !== board.empty) {
+        alert('Player ' + a + ' wins!');
+      }
+    }
+    return board.empty;
   }
-};
+
+  cells.click(function () {
+    if (turn() === playerOne) {
+      $(this).html('X');
+      board.state[($(this).data('num'))] = 1;
+      $(this).off('click');
+      count++;
+    } else {
+      $(this).html('O');
+      board.state[($(this).data('num'))] = 2;
+      $(this).off('click');
+      count++;
+    }
+    checkWin();
+  });
+})();
