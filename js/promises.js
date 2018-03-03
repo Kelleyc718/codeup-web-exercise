d88eab01f7b139fba82746170fe7da2672a70a00
Chris Kelley <kelleyc718@gmail.com>
2018-03-03 00:39:18 -0600

    Fixed algorithm for github file lister

--- js/promises.js
+++ js/promises.js
@@ -1,37 +1,43 @@
 'use strict';
-/* global prompt */
-/* global fetch */
-const userName = prompt('Enter your username');
-let repoList = ['codeup-web-exercises'];
-let fetchUrl = `https://api.github.com/repos/${userName}/${repoList}/contents`;
+{
+  const key = 'token ENTER YOUR TOKEN KEY HERE';
+  const userName = prompt('Enter your username');
+  let repoList = prompt('Enter the name of your repo');
+  let fetchUrl = `https://api.github.com/repos/${userName}/${repoList}/contents`;
 
-const isFile = path => {
-  return (path.match(/^.*\./g));
-};
+  const isFile = path => {
+    return (path.match(/^.*\./g));
+  };
 
-const files = [];
+  const files = [];
 
-const addFile = path => {
-  return files.push(path);
-};
+  const addFile = path => {
+    return files.push(path);
+  };
 
-const searcher = fetch(fetchUrl,
-  {headers: {'Authorization': 'token 8c1b2f1f1e09d3acbeab7cf3c7a8012c0a152666'}
-  }).then(response => response.json())
-    .then(data => handleFiles(data));
+  const searcher = fetchUrl => {
+    fetch(fetchUrl,
+    {headers: {'Authorization': `${key}`}})
+      .then(response => response.json())
+      .then(data => handleFiles(data));
+  };
 
-const handleFiles = data => {
-  let paths = data.map(({path}) => path);
-  console.log(paths);
-  for (let path of paths) {
-    if (isFile) {
-      addFile(path);
-    } else {
-      fetch(`${fetchUrl}/${path}`,
-        {headers: {'Authorization': 'token 8c1b2f1f1e09d3acbeab7cf3c7a8012c0a152666'}
-        }).then(handleFiles);
+  const handleFiles = data => {
+    let paths = data.map(({path}) => path);
+    for (let path of paths) {
+      if (isFile(path)) {
+        addFile(path);
+        htmlAdd(path);
+      } else {
+        searcher(`${fetchUrl}/${path}`);
+      }
     }
-  }
-};
+  };
 
-searcher(repoList);
+  const htmlAdd = path => {
+    document.getElementById('files').innerHTML += `${path}<br>`;
+  };
+
+  searcher(fetchUrl);
+  console.log(files);
+}
